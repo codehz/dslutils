e3535a04757f4c3bfd9b4e588bbfed4163bc82ad
CodeHz <codehz@outlook.com>
2021-10-22 14:36:49 +0800

    do not modify source expr

--- src/dslutils/identsubs.nim
+++ src/dslutils/identsubs.nim
@@ -2,13 +2,13 @@ import std/[tables, macros]
 
 func identsubs*(expr: NimNode, replacements: Table[string, NimNode]): NimNode =
   if expr.kind == nnkIdent:
-    copy replacements[expr.strVal]
+    return copy replacements[expr.strVal]
   elif expr.kind in nnkCallKinds and expr[0].kind in { nnkIdent, nnkSym }:
-    expr[1] = identsubs(expr[1], replacements)
-    expr
+    result = copy expr
+    result[1] = identsubs(expr[1], replacements)
   else:
-    expr[0] = identsubs(expr[0], replacements)
-    expr
+    result = copy expr
+    result[0] = identsubs(expr[0], replacements)
 
 func identsubs*(expr: NimNode, replacements: openArray[(string, NimNode)]): NimNode =
   expr.identsubs(replacements.toTable)
